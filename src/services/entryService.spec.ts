5144cc3e569921a160e3289b5dfafda2ee2efd39
pyabzz <>
2022-03-13 15:30:09 +0000

    Add unit testing

---
+++ src/services/entryService.spec.ts
@@ -0,0 +1,10 @@
+import { The } from '../refs'
+import { assert } from 'chai'
+
+describe(The.EntryService, () => {
+    describe('get', () => {
+        it('works', () => {
+            assert.equal(1, 1);
+        })
+    })
+});
