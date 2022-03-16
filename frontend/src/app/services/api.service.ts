7ac557d09bccaec6d6fae3d39a7706657574f049
pyabzz <>
2022-03-16 21:32:39 +0000

    Make apiService type agnostic

--- frontend/src/app/services/api.service.ts
+++ frontend/src/app/services/api.service.ts
@@ -1,6 +1,5 @@
 import { HttpClient } from '@angular/common/http'
 import { Injectable } from '@angular/core';
-import { Entry } from '../types/entry';
 import { ApiServiceConcept } from './api.service.concept';
 
 @Injectable({
@@ -15,7 +14,7 @@ export class ApiService implements ApiServiceConcept {
   get() {
     return new Promise<any>(
       (resolveCallback, rejectCallback) => {
-        const subscription = this.client.get<Entry[]>('http://localhost:3000') //todo: Should be type agnostic 'any'
+        const subscription = this.client.get<any>('http://localhost:3000')
           .subscribe({
             next: result => resolveCallback(result),
             error: er => { throw new Error(er) },
