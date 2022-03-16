21d521d3bce9b94be05872b7203cd08e62d184e6
pyabzz <>
2022-03-16 20:14:51 +0000

    Allow cors from the frontend on localhost

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -4,8 +4,15 @@ import { Config } from './config';
 import { Container } from './diContainer'
 import express, { Request, Response } from 'express';
 import { EntryServiceConcept } from "./services/entryServiceConcept";
+import cors from 'cors';
 
+const corsOptions = {
+    origin: 'http://localhost:4200',
+    methods: 'GET, POST'
+}
 const app = express();
+app.use(cors(corsOptions));
+
 app.get('/', (req: Request, res: Response) => {
     // todo: Resolve directly since there's no controller constructor involved
     const entryService: EntryServiceConcept = Container.resolve(The.EntryService);
